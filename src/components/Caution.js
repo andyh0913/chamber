55107b222c16cbb49d59b956f8f59663ee447c8e
Chih-Wei Fang <dodo0822@gmail.com>
2018-07-16 17:07:13 +0800

    add backend server

--- src/components/Caution.js
+++ src/components/Caution.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Caution extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      fetch('http://192.168.4.99/H');
+      setTimeout(() => {
+        fetch('http://192.168.4.99/L');
+      }, 2000);
+    }, 2000);
+  }
+
   render() {
     return (
       <div className="caution">
