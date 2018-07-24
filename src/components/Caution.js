7cf67cabbb5d7190d35abdfe8c6cc02a598d3f09
Chih-Wei Fang <dodo0822@gmail.com>
2018-07-24 23:06:52 +0800

    add explode

--- src/components/Caution.js
+++ src/components/Caution.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
 import bgm2URL from '../media/bgm2.ogg';
+import alarmURL from '../media/alert.ogg';
+import explodeURL from '../media/explode.ogg';
 
 export default class Caution extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      alarmPlayed: false,
+      explodePlayed: false
+    };
+  }
+
   componentDidMount() {
     setTimeout(() => {
       fetch('http://192.168.4.99/H');
@@ -11,6 +22,26 @@ export default class Caution extends Component {
     }, 2000);
     window.addEventListener("contextmenu", (e) => {e.preventDefault()});
     document.getElementById("bgm2").play();
+
+    setInterval(() => {
+      fetch('http://192.168.4.1/status').then((data) => {
+        return data.json();
+      }).then((data) => {
+        if(this.state.explodePlayed) return;
+        if(data.alarm && !this.state.alarmPlayed) {
+          this.setState({ alarmPlayed: true });
+          document.getElementById('alarm').play();
+        }
+        if(data.explode && !this.state.explodePlayed) {
+          this.setState({ explodePlayed: true });
+          document.getElementById('alarm').pause();
+          document.getElementById('bgm2').pause();
+          setTimeout(() => {
+            document.getElementById('explode').play();
+          }, 500);
+        }
+      })
+    }, 200);
   }
 
   render() {
@@ -19,6 +50,12 @@ export default class Caution extends Component {
         <h1>小心頭上</h1>
         <audio id="bgm2">
           <source src={bgm2URL} type="audio/ogg" />
+        </audio>
+        <audio id="alarm" loop="loop">
+          <source src={alarmURL} type="audio/ogg" />
+        </audio>
+        <audio id="explode">
+          <source src={explodeURL} type="audio/ogg" />
         </audio>
       </div>
     )
